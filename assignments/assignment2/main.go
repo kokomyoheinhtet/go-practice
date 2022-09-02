81746cb2de1095ee45889722dcb4874a599fe40b
KoKo Mhh <koko.mhh@lmwn.com>
2022-09-02 12:15:04 +0700

    added interface assignment

---
+++ assignments/assignment2/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type square struct {
+	sideLength float64
+}
+type triangle struct {
+	height float64
+	base   float64
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
+
+func main() {
+	s := square{sideLength: 5}
+	printArea(s)
+
+	t := triangle{height: 20, base: 15}
+	printArea(t)
+}
