7a8e175cb0efa484ecb2c67a767c61f7902457aa
Ko Ko Myo Hein Htet <koko.mhh@lmwn.com>
2022-12-08 09:06:10 +0700

    added liskov substitution principle

---
+++ designpatterns/liskov_substitution_principle/main/lsp.go
@@ -0,0 +1,72 @@
+package main
+
+import "fmt"
+
+type Sized interface {
+	GetWidth() int
+	SetWidth(width int)
+	GetHeight() int
+	SetHeight(height int)
+}
+
+type Rectangle struct {
+	width, height int
+}
+
+func (r *Rectangle) GetWidth() int {
+	return r.width
+}
+
+func (r *Rectangle) SetWidth(width int) {
+	r.width = width
+}
+
+func (r *Rectangle) GetHeight() int {
+	return r.height
+}
+
+func (r *Rectangle) SetHeight(height int) {
+	r.height = height
+}
+
+// modified LSP
+// If a function takes an interface and
+// works with a type T that implements this
+// interface, any structure that aggregates T
+// should also be usable in that function.
+type Square struct {
+	Rectangle
+}
+
+func NewSquare(size int) *Square {
+	sq := Square{}
+	sq.width = size
+	sq.height = size
+	return &sq
+}
+
+func (sq *Square) SetWidth(width int) {
+	sq.width = width
+	sq.height = width
+}
+
+func (sq *Square) SetHeight(height int) {
+	sq.width = height
+	sq.height = height
+}
+
+func UseIt(sized Sized) {
+	width := sized.GetWidth()
+	sized.SetHeight(10)
+	expectedArea := 10 * width
+	actualArea := sized.GetHeight() * sized.GetWidth()
+	fmt.Println("Expected area", expectedArea, " but got area", actualArea)
+}
+
+func main() {
+	rc := &Rectangle{2, 3}
+	UseIt(rc)
+
+	sq := NewSquare(5)
+	UseIt(sq)
+}
